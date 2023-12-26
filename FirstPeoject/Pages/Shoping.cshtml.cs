265de8718604ba92428ecd82f3bcfb3a753fb38e
Alireza <69059180+alireza-flhz@users.noreply.github.com>
2023-12-26 18:40:16 +0330

    Add WEbsite ( UI ) shopping and Personal

---
+++ FirstPeoject/Pages/Shoping.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstPeoject.Pages
+{
+    public class ShopingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
